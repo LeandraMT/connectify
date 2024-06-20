f687055646995390abed0ec447df9dc83c4acfb8
LeandraMT <magantierleandra@gmail.com>
2024-06-20 09:14:12 +0200

    added env variable for secret key

--- connectify/settings.py
+++ connectify/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0*!evtm*)o440dik0z)(g_#mux1doxa68&tp@(6lze*&tn&bj1'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-0*!evtm*)o440dik0z)(g_#mux1doxa68&tp@(6lze*&tn&bj1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['connectify-lovat-gamma.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
